b4d7110be0c753945b53a7dfeacc93651826e7db
Selton Guedes <sfgs@ecomp.poli.br>
2021-12-07 13:16:42 -0300

    LPS-138925 Add new delete transition method

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-api/src/main/java/com/liferay/portal/workflow/metrics/search/index/TransitionWorkflowMetricsIndexer.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-api/src/main/java/com/liferay/portal/workflow/metrics/search/index/TransitionWorkflowMetricsIndexer.java
@@ -16,6 +16,7 @@ package com.liferay.portal.workflow.metrics.search.index;
 
 import com.liferay.portal.search.document.Document;
 import com.liferay.portal.workflow.metrics.model.AddTransitionRequest;
+import com.liferay.portal.workflow.metrics.model.DeleteTransitionRequest;
 
 /**
  * @author Rafael Praxedes
@@ -23,6 +24,13 @@ import com.liferay.portal.workflow.metrics.model.AddTransitionRequest;
 public interface TransitionWorkflowMetricsIndexer {
 
 	public Document addTransition(AddTransitionRequest addTransitionRequest);
+
+	public default void deleteTransition(
+		DeleteTransitionRequest deleteTransitionRequest) {
+
+		throw new UnsupportedOperationException(
+			"This method needs to be implemented");
+	}
 
 	public void deleteTransition(long companyId, long transitionId);
 
