d6fcca27ca4c1a9cc28e94bf3ebf1b6c3a469911
Selton Guedes <sfgs@ecomp.poli.br>
2022-04-11 15:42:17 -0300

    LPS-150829 Add edit object validation MVC render

--- modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/dto/v1_0/util/ObjectValidationRuleUtil.java
+++ modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/dto/v1_0/util/ObjectValidationRuleUtil.java
@@ -16,9 +16,7 @@ package com.liferay.object.admin.rest.internal.dto.v1_0.util;
 
 import com.liferay.object.admin.rest.dto.v1_0.ObjectValidationRule;
 import com.liferay.object.util.LocalizedMapUtil;
-import com.liferay.portal.kernel.language.LanguageUtil;
 
-import java.util.Locale;
 import java.util.Map;
 
 /**
@@ -28,7 +26,6 @@ public class ObjectValidationRuleUtil {
 
 	public static ObjectValidationRule toObjectValidationRule(
 		Map<String, Map<String, String>> actions,
-		Locale locale,
 		com.liferay.object.model.ObjectValidationRule
 			serviceBuilderObjectValidationRule) {
 
@@ -44,8 +41,6 @@ public class ObjectValidationRuleUtil {
 				dateModified =
 					serviceBuilderObjectValidationRule.getModifiedDate();
 				engine = serviceBuilderObjectValidationRule.getEngine();
-				engineLabel = LanguageUtil.get(
-					locale, serviceBuilderObjectValidationRule.getEngine());
 				errorLabel = LocalizedMapUtil.getLanguageIdMap(
 					serviceBuilderObjectValidationRule.getErrorLabelMap());
 				id =
