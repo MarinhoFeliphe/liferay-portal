e0b12c0031a5d665949ed21f302cb2dd16f65e9a
Jair Medeiros <jair.medeiros@liferay.com>
2022-06-18 21:43:50 -0300

    LPS-152403 Fix code to use custom fields filter correctly

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/components/ActivationStatus/DXPCloud/index.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/components/ActivationStatus/DXPCloud/index.js
@@ -211,7 +211,7 @@ const ActivationStatusDXPCloud = ({
 
 	useEffect(() => {
 		const fetchCommerceOrderItems = async () => {
-			const filterAccountSubscriptionERC = `customFields/name eq 'accountSubscriptionGroupERC' and customFields/customValue/data eq '${project.accountKey}_dxp-cloud'`;
+			const filterAccountSubscriptionERC = `customFields/accountSubscriptionGroupERC eq '${project.accountKey}_dxp-cloud'`;
 			const {data} = await client.query({
 				query: getCommerceOrderItems,
 				variables: {
