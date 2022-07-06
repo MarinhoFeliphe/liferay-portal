aa32e7e00db42e9bfb6687b09ffc2f2a8514a063
Feliphe Marinho <feliphe.marinho@liferay.com>
2022-07-06 14:34:42 -0300

    LPS-157642 Create ObjectFieldSerializer and apply to use

--- modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/definitions/display/context/ObjectDefinitionsStateManagerDisplayContext.java
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/definitions/display/context/ObjectDefinitionsStateManagerDisplayContext.java
@@ -18,18 +18,17 @@ import com.liferay.frontend.data.set.model.FDSActionDropdownItem;
 import com.liferay.frontend.taglib.clay.servlet.taglib.util.CreationMenu;
 import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.model.ObjectField;
+import com.liferay.object.web.internal.object.definitions.display.context.util.ObjectFieldSerializerUtil;
 import com.liferay.petra.portlet.url.builder.PortletURLBuilder;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONObject;
-import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.portlet.LiferayWindowState;
 import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Collections;
 import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Carlos Montenegro
@@ -68,15 +67,8 @@ public class ObjectDefinitionsStateManagerDisplayContext
 				"get", null, "sidePanel"));
 	}
 
-	public JSONObject getObjectStateFlowJSONObject(ObjectField objectField) {
-		return JSONUtil.put(
-			"label", objectField.getLabelMap()
-		).put(
-			"listTypeDefinitionId",
-			Long.valueOf(objectField.getListTypeDefinitionId())
-		).put(
-			"objectFieldId", objectField.getObjectFieldId()
-		);
+	public JSONObject getObjectFieldJSONObject(ObjectField objectField) {
+		return ObjectFieldSerializerUtil.serialize(objectField);
 	}
 
 	@Override
