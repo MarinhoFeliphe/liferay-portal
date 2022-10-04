b981e6a3fc322001eecab4d81bb693f694430568
Feliphe Marinho <feliphe.marinho@liferay.com>
2022-10-04 11:36:20 -0300

    Pass permissionChecker

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -102,6 +102,7 @@ import com.liferay.portal.kernel.search.IndexerRegistryUtil;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
 import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
 import com.liferay.portal.kernel.security.permission.InlineSQLHelper;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
 import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.service.PersistedModelLocalService;
@@ -602,12 +603,14 @@ public class ObjectEntryLocalServiceImpl
 
 	public List<ObjectEntry> getOneToManyObjectEntries(
 			long groupId, long objectRelationshipId, long primaryKey,
-			boolean related, int start, int end)
+			boolean related, int start, int end,
+			PermissionChecker permissionChecker)
 		throws PortalException {
 
 		DSLQuery dslQuery = _getOneToManyObjectEntriesGroupByStep(
-			groupId, objectRelationshipId, primaryKey, related,
-			DSLQueryFactoryUtil.selectDistinct(ObjectEntryTable.INSTANCE)
+			groupId, objectRelationshipId, primaryKey,
+			related, DSLQueryFactoryUtil.selectDistinct(
+				ObjectEntryTable.INSTANCE), permissionChecker
 		).limit(
 			start, end
 		);
@@ -622,13 +625,13 @@ public class ObjectEntryLocalServiceImpl
 	@Override
 	public int getOneToManyObjectEntriesCount(
 			long groupId, long objectRelationshipId, long primaryKey,
-			boolean related)
+			boolean related, PermissionChecker permissionChecker)
 		throws PortalException {
 
 		DSLQuery dslQuery = _getOneToManyObjectEntriesGroupByStep(
 			groupId, objectRelationshipId, primaryKey, related,
 			DSLQueryFactoryUtil.countDistinct(
-				ObjectEntryTable.INSTANCE.objectEntryId));
+				ObjectEntryTable.INSTANCE.objectEntryId), permissionChecker);
 
 		if (_log.isDebugEnabled()) {
 			_log.debug(
@@ -1663,8 +1666,8 @@ public class ObjectEntryLocalServiceImpl
 	}
 
 	private GroupByStep _getOneToManyObjectEntriesGroupByStep(
-			long groupId, long objectRelationshipId, long primaryKey,
-			boolean related, FromStep fromStep)
+		long groupId, long objectRelationshipId, long primaryKey,
+		boolean related, FromStep fromStep, PermissionChecker permissionChecker)
 		throws PortalException {
 
 		ObjectRelationship objectRelationship =
@@ -1737,7 +1740,7 @@ public class ObjectEntryLocalServiceImpl
 				}
 			).and(
 				() -> {
-					if (PermissionThreadLocal.getPermissionChecker() == null) {
+					if (permissionChecker == null) {
 						return null;
 					}
 
