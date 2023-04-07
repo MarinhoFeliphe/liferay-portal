68a175da4580a2781f9ed425bd9efccc2c8491d1
Feliphe Marinho <feliphemarinho18@gmail.com>
2023-04-07 02:00:29 -0300

    LPS-179179 Remove unnecessary methods

--- modules/apps/object/object-service/src/main/java/com/liferay/object/model/impl/ObjectFieldImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/model/impl/ObjectFieldImpl.java
@@ -53,105 +53,10 @@ public class ObjectFieldImpl extends ObjectFieldBaseImpl {
 	}
 
 	@Override
-	public boolean isAttachment() {
-		if (Objects.equals(
-				getBusinessType(),
-				ObjectFieldConstants.BUSINESS_TYPE_ATTACHMENT)) {
-
-			return true;
-		}
-
-		return false;
-	}
-
-	@Override
-	public boolean isBoolean() {
-		if (Objects.equals(
-				getBusinessType(),
-				ObjectFieldConstants.BUSINESS_TYPE_BOOLEAN)) {
-
-			return true;
-		}
-
-		return false;
-	}
-
-	@Override
-	public boolean isDate() {
-		if (Objects.equals(
-				getBusinessType(), ObjectFieldConstants.BUSINESS_TYPE_DATE)) {
-
-			return true;
-		}
-
-		return false;
-	}
-
-	@Override
-	public boolean isDecimal() {
-		if (Objects.equals(
-				getBusinessType(),
-				ObjectFieldConstants.BUSINESS_TYPE_DECIMAL)) {
-
-			return true;
-		}
-
-		return false;
-	}
-
-	@Override
 	public boolean isFormula() {
 		if (Objects.equals(
 				getBusinessType(),
 				ObjectFieldConstants.BUSINESS_TYPE_FORMULA)) {
-
-			return true;
-		}
-
-		return false;
-	}
-
-	@Override
-	public boolean isInteger() {
-		if (Objects.equals(
-				getBusinessType(),
-				ObjectFieldConstants.BUSINESS_TYPE_INTEGER)) {
-
-			return true;
-		}
-
-		return false;
-	}
-
-	@Override
-	public boolean isLargeFile() {
-		if (Objects.equals(
-				getBusinessType(),
-				ObjectFieldConstants.BUSINESS_TYPE_LARGE_FILE)) {
-
-			return true;
-		}
-
-		return false;
-	}
-
-	@Override
-	public boolean isLongInteger() {
-		if (Objects.equals(
-				getBusinessType(),
-				ObjectFieldConstants.BUSINESS_TYPE_LONG_INTEGER)) {
-
-			return true;
-		}
-
-		return false;
-	}
-
-	@Override
-	public boolean isLongText() {
-		if (Objects.equals(
-				getBusinessType(),
-				ObjectFieldConstants.BUSINESS_TYPE_LONG_TEXT)) {
 
 			return true;
 		}
@@ -172,33 +77,10 @@ public class ObjectFieldImpl extends ObjectFieldBaseImpl {
 	}
 
 	@Override
-	public boolean isPrecisionDecimal() {
-		if (Objects.equals(
-				getBusinessType(),
-				ObjectFieldConstants.BUSINESS_TYPE_PRECISION_DECIMAL)) {
-
-			return true;
-		}
-
-		return false;
-	}
-
-	@Override
 	public boolean isRelationship() {
 		if (Objects.equals(
 				getBusinessType(),
 				ObjectFieldConstants.BUSINESS_TYPE_RELATIONSHIP)) {
-
-			return true;
-		}
-
-		return false;
-	}
-
-	@Override
-	public boolean isText() {
-		if (Objects.equals(
-				getBusinessType(), ObjectFieldConstants.BUSINESS_TYPE_TEXT)) {
 
 			return true;
 		}
