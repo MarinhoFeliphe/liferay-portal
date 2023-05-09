20e02348871ce93e4bac59d90234fe150537ffdb
Feliphe Marinho <feliphemarinho18@gmail.com>
2023-05-09 14:19:16 -0300

    LPS-176921 Create getI18nObjectFieldName method in ObjectFieldImpl and apply to use

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -1562,8 +1562,7 @@ public class ObjectEntryLocalServiceImpl
 
 		for (ObjectField objectField : objectFields) {
 			if (objectField.isRequired() &&
-				!values.containsKey(
-					objectField.getName() + ObjectFieldConstants.I18N_SUFFIX)) {
+				!values.containsKey(objectField.getI18nObjectFieldName())) {
 
 				throw new ObjectEntryValuesException.Required(
 					objectField.getName());
@@ -1692,8 +1691,7 @@ public class ObjectEntryLocalServiceImpl
 
 				Map<String, String> localizedValues =
 					(Map<String, String>)values.get(
-						objectField.getName() +
-							ObjectFieldConstants.I18N_SUFFIX);
+						objectField.getI18nObjectFieldName());
 
 				if (localizedValues == null) {
 					_setColumn(
@@ -3007,20 +3005,18 @@ public class ObjectEntryLocalServiceImpl
 		for (ObjectField objectField :
 				dynamicObjectDefinitionLocalizationTable.getObjectFields()) {
 
-			String localizedObjectFieldName =
-				objectField.getName() + ObjectFieldConstants.I18N_SUFFIX;
-
-			if (!values.containsKey(localizedObjectFieldName)) {
+			if (!values.containsKey(objectField.getI18nObjectFieldName())) {
 				continue;
 			}
 
 			Map<String, String> localizedValues =
-				(Map<String, String>)values.get(localizedObjectFieldName);
+				(Map<String, String>)values.get(
+					objectField.getI18nObjectFieldName());
 
 			for (Map.Entry<String, String> entry : localizedValues.entrySet()) {
 				Map<String, Serializable> localizedValue =
 					HashMapBuilder.<String, Serializable>put(
-						localizedObjectFieldName,
+						objectField.getI18nObjectFieldName(),
 						(Serializable)HashMapBuilder.put(
 							entry.getKey(), entry.getValue()
 						).build()
@@ -3528,9 +3524,7 @@ public class ObjectEntryLocalServiceImpl
 			dynamicObjectDefinitionLocalizationTable.getObjectFields();
 
 		for (ObjectField objectField : objectFields) {
-			if (!values.containsKey(
-					objectField.getName() + ObjectFieldConstants.I18N_SUFFIX)) {
-
+			if (!values.containsKey(objectField.getI18nObjectFieldName())) {
 				continue;
 			}
 
@@ -3574,10 +3568,7 @@ public class ObjectEntryLocalServiceImpl
 			int index = 1;
 
 			for (ObjectField objectField : objectFields) {
-				if (!values.containsKey(
-						objectField.getName() +
-							ObjectFieldConstants.I18N_SUFFIX)) {
-
+				if (!values.containsKey(objectField.getI18nObjectFieldName())) {
 					continue;
 				}
 
@@ -3587,8 +3578,7 @@ public class ObjectEntryLocalServiceImpl
 
 				Map<String, String> localizedValues =
 					(Map<String, String>)values.get(
-						objectField.getName() +
-							ObjectFieldConstants.I18N_SUFFIX);
+						objectField.getI18nObjectFieldName());
 
 				String localizedValue = localizedValues.get(languageId);
 
