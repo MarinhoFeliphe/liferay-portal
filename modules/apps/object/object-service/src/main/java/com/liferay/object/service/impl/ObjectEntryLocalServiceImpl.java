2616066ac62ded1218ad7e9c9b58e589febaa95a
Feliphe Marinho <feliphemarinho18@gmail.com>
2023-05-22 14:28:03 -0300

    LPS-176921 Check required fields on insert

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -2984,7 +2984,12 @@ public class ObjectEntryLocalServiceImpl
 				(Map<String, String>)values.get(
 					objectField.getI18nObjectFieldName());
 
-			if (localizedValues == null) {
+			if ((localizedValues == null) || localizedValues.isEmpty()) {
+				if (objectField.isRequired()) {
+					throw new ObjectEntryValuesException.Required(
+						objectField.getName());
+				}
+
 				continue;
 			}
 
@@ -3085,6 +3090,12 @@ public class ObjectEntryLocalServiceImpl
 						return (Serializable)value;
 					});
 			}
+		}
+
+		if ((newI18nObjectFieldValues == null) ||
+			newI18nObjectFieldValues.isEmpty()) {
+
+			return;
 		}
 
 		_insertIntoLocalizationTable(
