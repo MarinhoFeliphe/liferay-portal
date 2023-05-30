fa0a7afc7d79074ba4ddda1b8de2664634880d07
Brian Chan <brian.chan@liferay.com>
2023-05-30 11:20:36 +0530

    LPS-176921 As used

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -2914,11 +2914,11 @@ public class ObjectEntryLocalServiceImpl
 						dynamicObjectDefinitionLocalizationTable.getColumn(
 							objectField.getDBColumnName());
 
+					String value = StringPool.BLANK;
+
 					Map<String, String> localizedValues =
 						(Map<String, String>)values.get(
 							objectField.getI18nObjectFieldName());
-
-					String value = StringPool.BLANK;
 
 					if (localizedValues != null) {
 						value = Objects.toString(
