aca67aa788b8c989176e0f1f80582d752d517452
Brian Chan <brian.chan@liferay.com>
2023-06-30 12:49:14 +0530

    LPS-184523 Spell it out

--- modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/info/collection/provider/SXPBlueprintInfoCollectionProvider.java
+++ modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/info/collection/provider/SXPBlueprintInfoCollectionProvider.java
@@ -250,7 +250,7 @@ public class SXPBlueprintInfoCollectionProvider
 					}
 
 					searchContext.setAttribute(
-						"search.experiences.blueprint.erc",
+						"search.experiences.blueprint.external.reference.code",
 						_sxpBlueprint.getExternalReferenceCode());
 					searchContext.setAttribute(
 						"search.experiences.ip.address",
