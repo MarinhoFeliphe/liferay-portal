f67d65fa5e7fe6c675dbbe8abc2dc7799e4a0cb0
MarinhoFeliphe <feliphemarinho18@gmail.com>
2023-10-16 15:52:24 -0300

    LPS-195703 Rename to createObjectDefinitionTree

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectEntryLocalServiceTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectEntryLocalServiceTest.java
@@ -2733,7 +2733,7 @@ public class ObjectEntryLocalServiceTest {
 	private void _testAddOrUpdateObjectEntryRootObjectEntryId()
 		throws Exception {
 
-		Tree tree = TreeTestUtil.createTree(
+		Tree tree = TreeTestUtil.createObjectDefinitionTree(
 			_objectDefinitionLocalService, _objectRelationshipLocalService,
 			_treeFactory);
 
