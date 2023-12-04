fde14313f9f894c44467692fe114c31dff0ad508
MarinhoFeliphe <feliphemarinho18@gmail.com>
2023-12-04 11:01:09 -0300

    LPS-202448 SF

--- modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/LeftSidebar/LeftSidebar.tsx
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/LeftSidebar/LeftSidebar.tsx
@@ -137,7 +137,6 @@ export default function LeftSidebar({setShowModal}: LeftSidebarProps) {
 							!!leftSidebarItems.length && (
 								<>
 									<LeftSidebarTreeView
-
 										expandedKeys={expandedKeys}
 										leftSidebarOtherObjectFoldersItems={
 											leftSidebarOtherObjectFoldersItems
