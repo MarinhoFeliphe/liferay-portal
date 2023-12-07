be06a307d58d3c9a2fc9f8b5c6dbc546227bb288
MarinhoFeliphe <feliphemarinho18@gmail.com>
2023-12-07 19:06:46 -0300

    WIP

---
+++ portal-kernel/test/unit/com/liferay/portal/kernel/workflow/DefaultWorkflowNodeTest.java
@@ -0,0 +1,24 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.kernel.workflow;
+
+import org.junit.Test;
+
+/**
+ * @author Feliphe Marinho
+ * @author Paulo Albuquerque
+ */
+public class DefaultWorkflowNodeTest {
+
+	@Test
+	public void testGetLabel() {
+		// Desired locale, label map
+		// Desired locale, name as language key
+		// Site default locale, label map
+		// Site default local, label map
+		// Last option
+	}
+}
