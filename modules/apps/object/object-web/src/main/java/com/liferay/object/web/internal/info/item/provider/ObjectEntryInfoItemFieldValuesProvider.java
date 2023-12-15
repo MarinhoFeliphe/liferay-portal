6a411b7c40c2262a1b39d3cab367d0153c3522b5
Selton Guedes <selton.guedes@liferay.com>
2023-12-15 16:32:07 -0300

    LPS-203970 Remove unused TimeZone

--- modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/info/item/provider/ObjectEntryInfoItemFieldValuesProvider.java
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/info/item/provider/ObjectEntryInfoItemFieldValuesProvider.java
@@ -603,8 +603,7 @@ public class ObjectEntryInfoItemFieldValuesProvider
 		throws Exception {
 
 		Object value = ObjectEntryUtil.getValue(
-			themeDisplay.getLocale(), objectField, themeDisplay.getTimeZone(),
-			objectEntry.getProperties());
+			themeDisplay.getLocale(), objectField, objectEntry.getProperties());
 
 		if (value == null) {
 			return StringPool.BLANK;
--- modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/util/ObjectEntryUtil.java
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/util/ObjectEntryUtil.java
@@ -45,7 +45,6 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
-import java.util.TimeZone;
 
 /**
  * @author Eudaldo Alonso
@@ -74,8 +73,7 @@ public class ObjectEntryUtil {
 	}
 
 	public static Object getValue(
-			Locale locale, ObjectField objectField, TimeZone timeZone,
-			Map<String, Object> values)
+			Locale locale, ObjectField objectField, Map<String, Object> values)
 		throws Exception {
 
 		Object value = values.get(objectField.getName());
