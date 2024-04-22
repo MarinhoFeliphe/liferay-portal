666171f3734813fe24d3cf8a45915e471af1714f
MarinhoFeliphe <feliphemarinho18@gmail.com>
2024-04-22 18:06:59 -0300

    LPD-23693 The default value of the configuration resposible to allow unsafe scripts should be false when there is no unsafe script being used

--- modules/apps/portal-security/portal-security-script-management-web/src/main/java/com/liferay/portal/security/script/management/web/internal/configuration/helper/ScriptManagementConfigurationHelperImpl.java
+++ modules/apps/portal-security/portal-security-script-management-web/src/main/java/com/liferay/portal/security/script/management/web/internal/configuration/helper/ScriptManagementConfigurationHelperImpl.java
@@ -9,20 +9,26 @@ import com.liferay.configuration.admin.constants.ConfigurationAdminPortletKeys;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.portal.configuration.module.configuration.ConfigurationProvider;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.instance.PortalInstancePool;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.GroupConstants;
 import com.liferay.portal.kernel.module.configuration.ConfigurationException;
 import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
 import com.liferay.portal.kernel.util.HttpComponentsUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.security.script.management.configuration.ScriptManagementConfiguration;
 import com.liferay.portal.security.script.management.configuration.helper.ScriptManagementConfigurationHelper;
+import com.liferay.portal.security.script.management.groovy.script.use.GroovyScriptUse;
+import com.liferay.portal.security.script.management.web.internal.groovy.script.uses.factory.GroovyScriptUsesFactoryRegistry;
 import com.liferay.portal.util.PropsValues;
 
 import java.io.IOException;
 
+import java.util.List;
 import java.util.Map;
 
 import javax.portlet.PortletMode;
@@ -109,8 +115,30 @@ public class ScriptManagementConfigurationHelperImpl
 	}
 
 	@Activate
+	protected void activate(Map<String, Object> properties) throws Exception {
+		modified(properties);
+
+		if (!_systemScriptManagementConfiguration.
+				allowScriptContentToBeExecutedOrIncluded()) {
+
+			return;
+		}
+
+		List<GroovyScriptUse> groovyScriptUses =
+			_groovyScriptUsesFactoryRegistry.getGroovyScriptUses(
+				LocaleUtil.fromLanguageId(PropsValues.COMPANY_DEFAULT_LOCALE));
+
+		if (groovyScriptUses.isEmpty()) {
+			_configurationProvider.saveSystemConfiguration(
+				ScriptManagementConfiguration.class,
+				HashMapDictionaryBuilder.<String, Object>put(
+					"allowScriptContentToBeExecutedOrIncluded", false
+				).build());
+		}
+	}
+
 	@Modified
-	protected void activate(Map<String, Object> properties) {
+	protected void modified(Map<String, Object> properties) {
 		_systemScriptManagementConfiguration =
 			ConfigurableUtil.createConfigurable(
 				ScriptManagementConfiguration.class, properties);
@@ -121,6 +149,12 @@ public class ScriptManagementConfigurationHelperImpl
 
 	@Reference
 	private ConfigurationAdmin _configurationAdmin;
+
+	@Reference
+	private ConfigurationProvider _configurationProvider;
+
+	@Reference
+	private GroovyScriptUsesFactoryRegistry _groovyScriptUsesFactoryRegistry;
 
 	@Reference
 	private Portal _portal;
