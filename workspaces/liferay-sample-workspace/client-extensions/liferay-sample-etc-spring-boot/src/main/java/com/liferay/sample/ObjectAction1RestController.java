081d5a90850cb3cf5b142fe9f110b2bbab81b7e9
Feliphe Marinho <feliphemarinho18@gmail.com>
2024-08-02 13:49:56 -0300

    Sample client extension based object action

--- workspaces/liferay-sample-workspace/client-extensions/liferay-sample-etc-spring-boot/src/main/java/com/liferay/sample/ObjectAction1RestController.java
+++ workspaces/liferay-sample-workspace/client-extensions/liferay-sample-etc-spring-boot/src/main/java/com/liferay/sample/ObjectAction1RestController.java
@@ -5,17 +5,18 @@
 
 package com.liferay.sample;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author Raymond Augé
@@ -28,9 +29,34 @@ public class ObjectAction1RestController extends BaseRestController {
 
 	@PostMapping
 	public ResponseEntity<String> post(
-		@AuthenticationPrincipal Jwt jwt, @RequestBody String json) {
+		@AuthenticationPrincipal Jwt jwt, @RequestBody String json) throws Exception {
 
 		log(jwt, _log, json);
+
+		// get book id from the payload
+
+		JsonNode jsonNode = new ObjectMapper().readTree(json);
+
+		jsonNode = jsonNode.get("objectEntryDTOBook");
+		jsonNode = jsonNode.get("id");
+
+		// perform a GET request to obtain the authors of that book,
+		// remember to set the oauth2 scope for book object definition
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setBearerAuth(jwt.getTokenValue());
+
+		ResponseEntity<String> response = new RestTemplate().exchange(
+				lxcDXPServerProtocol + "://" + lxcDXPMainDomain +
+						"/o/c/books/" + jsonNode.asLong() + "/bookAuthor", HttpMethod.GET,
+				new HttpEntity<>(headers), String.class);
+
+		jsonNode = new ObjectMapper().readTree(response.getBody());
+		jsonNode.get("items");
+
+		// iterate over the items that will contain the authors of that book.
+
+		// send a notification different from user-notification
 
 		return new ResponseEntity<>(json, HttpStatus.OK);
 	}
