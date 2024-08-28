3b0bdddb90cb44dc60706c889f4c6f9f8f0fb669
Feliphe Marinho <feliphemarinho18@gmail.com>
2024-08-28 11:36:14 -0300

    LPD-34797 No need to put languageId and the localized values at this point

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -3576,7 +3576,7 @@ public class ObjectEntryLocalServiceImpl
 					columnNames, index++, insertedValues, preparedStatement,
 					Types.BIGINT, objectEntryId);
 				_setColumn(
-					columnNames, index++, insertedValues, preparedStatement,
+					columnNames, index++, new HashMap<>(), preparedStatement,
 					Types.VARCHAR, languageId);
 
 				for (ObjectField objectField : objectFields) {
@@ -3585,8 +3585,8 @@ public class ObjectEntryLocalServiceImpl
 							objectField.getDBColumnName());
 
 					_setColumn(
-						columnNames, index++, insertedValues, preparedStatement,
-						column.getSQLType(),
+						columnNames, index++, new HashMap<>(),
+						preparedStatement, column.getSQLType(),
 						_getLocalizedValue(
 							languageId,
 							(Map<String, String>)values.get(
