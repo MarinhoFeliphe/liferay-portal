f8272680caa477d4eba8b0fdd8190fe9575f8011
Brian Chan <brian.chan@liferay.com>
2025-08-25 14:51:22 +0900

    LPD-62771 As used

--- modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/manager/v1_0/DefaultObjectEntryManagerImpl.java
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/manager/v1_0/DefaultObjectEntryManagerImpl.java
@@ -1250,15 +1250,6 @@ public class DefaultObjectEntryManagerImpl
 			String parentExternalReferenceCode, String scopeKey)
 		throws Exception {
 
-		com.liferay.object.model.ObjectEntry parentServiceBuilderObjectEntry =
-			_objectEntryService.getObjectEntry(
-				parentExternalReferenceCode,
-				getGroupId(
-					_objectDefinitionLocalService.getObjectDefinition(
-						objectRelationship.getObjectDefinitionId1()),
-					scopeKey),
-				objectRelationship.getObjectDefinitionId1());
-
 		com.liferay.object.model.ObjectEntry serviceBuilderObjectEntry =
 			_objectEntryService.getObjectEntry(
 				externalReferenceCode,
@@ -1267,6 +1258,14 @@ public class DefaultObjectEntryManagerImpl
 						objectRelationship.getObjectDefinitionId2()),
 					scopeKey),
 				objectRelationship.getObjectDefinitionId2());
+		com.liferay.object.model.ObjectEntry parentServiceBuilderObjectEntry =
+			_objectEntryService.getObjectEntry(
+				parentExternalReferenceCode,
+				getGroupId(
+					_objectDefinitionLocalService.getObjectDefinition(
+						objectRelationship.getObjectDefinitionId1()),
+					scopeKey),
+				objectRelationship.getObjectDefinitionId1());
 
 		return _updateRelatedObjectEntry(
 			dtoConverterContext, objectEntry,
