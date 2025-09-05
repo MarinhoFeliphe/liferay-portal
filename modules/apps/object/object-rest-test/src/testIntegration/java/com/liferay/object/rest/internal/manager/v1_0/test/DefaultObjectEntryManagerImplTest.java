d1504b1f7bc609db7e233dc1253527a14054459b
Feliphe Marinho <feliphemarinho18@gmail.com>
2025-09-05 17:10:21 -0300

    LPD-60127 test: fix tests

--- modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/manager/v1_0/test/DefaultObjectEntryManagerImplTest.java
+++ modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/manager/v1_0/test/DefaultObjectEntryManagerImplTest.java
@@ -1207,9 +1207,6 @@ public class DefaultObjectEntryManagerImplTest
 			_objectRelationshipLocalService.getObjectRelationship(
 				edge.getObjectRelationshipId());
 
-		ObjectField objectField = objectFieldLocalService.getObjectField(
-			objectRelationship.getObjectFieldId2());
-
 		_defaultObjectEntryManager.addRelatedObjectEntry(
 			_simpleDTOConverterContext,
 			new ObjectEntry() {
@@ -1248,18 +1245,14 @@ public class DefaultObjectEntryManagerImplTest
 				"User ", _user.getUserId(),
 				" must have ADD_OBJECT_ENTRY permission for ",
 				_rootObjectDefinition.getResourceName(), StringPool.SPACE),
-			() -> _defaultObjectEntryManager.addObjectEntry(
+			() -> _defaultObjectEntryManager.addRelatedObjectEntry(
 				_simpleDTOConverterContext,
-				objectDefinitionLocalService.getObjectDefinition(
-					childNode.getPrimaryKey()),
 				new ObjectEntry() {
 					{
-						properties = HashMapBuilder.<String, Object>put(
-							objectField.getName(), objectEntry.getId()
-						).build();
+						properties = new HashMap<>();
 					}
 				},
-				ObjectDefinitionConstants.SCOPE_COMPANY));
+				objectEntry.getId(), objectRelationship));
 		AssertUtils.assertFailure(
 			PrincipalException.MustHavePermission.class,
 			StringBundler.concat(
@@ -9451,12 +9444,10 @@ public class DefaultObjectEntryManagerImplTest
 			_createDTOConverterContext(), objectDefinitionAA, objectEntry,
 			scopeKey);
 
-		Assert.assertEquals(
-			0L,
+		Assert.assertNull(
 			objectEntryAA.getPropertyValue(
 				objectRelationshipA_AAObjectField2.getName()));
-		Assert.assertEquals(
-			0L,
+		Assert.assertNull(
 			objectEntryAA.getPropertyValue(
 				objectRelationshipB_AAObjectField2.getName()));
 
@@ -9477,8 +9468,7 @@ public class DefaultObjectEntryManagerImplTest
 			objectEntryA.getId(),
 			objectEntryAA.getPropertyValue(
 				objectRelationshipA_AAObjectField2.getName()));
-		Assert.assertEquals(
-			0L,
+		Assert.assertNull(
 			objectEntryAA.getPropertyValue(
 				objectRelationshipB_AAObjectField2.getName()));
 
@@ -9722,7 +9712,7 @@ public class DefaultObjectEntryManagerImplTest
 		assertEquals(
 			unsafeTriFunction.apply(
 				null, null, getSorts("textObjectFieldName:desc")),
-			Page.of(List.of(objectEntryAA2, objectEntryAA1), null, 1));
+			Page.of(List.of(objectEntryAA2, objectEntryAA1), null, 2));
 
 		_objectEntryLocalService.deleteObjectEntry(objectEntryAA1.getId());
 		_objectEntryLocalService.deleteObjectEntry(objectEntryAA2.getId());
@@ -10061,8 +10051,7 @@ public class DefaultObjectEntryManagerImplTest
 			objectEntryA.getId(),
 			objectEntry.getPropertyValue(
 				objectRelationshipA_AAObjectField2.getName()));
-		Assert.assertEquals(
-			0L,
+		Assert.assertNull(
 			objectEntry.getPropertyValue(
 				objectRelationshipB_AAObjectField2.getName()));
 
