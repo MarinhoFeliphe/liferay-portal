7f34a2c95d8e901af146a79e400b818bd97e207a
Davyson Melo <davyson.melo@liferay.com>
2025-11-12 11:33:51 -0300

    LPD-71144 Add prompt input to the section

--- modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/components/sidebar/sections/prompt/PromptSummary.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/components/sidebar/sections/prompt/PromptSummary.js
@@ -3,14 +3,32 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import {ClayInput} from '@clayui/form';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, {useContext} from 'react';
 
+import {DiagramBuilderContext} from '../../../../DiagramBuilderContext';
 import SidebarPanel from '../../SidebarPanel';
 
-const PromptSummary = ({setContentName}) => {
+const PromptSummary = () => {
+	const {selectedItem, setSelectedItem} = useContext(DiagramBuilderContext);
+
 	return (
 		<SidebarPanel panelTitle={Liferay.Language.get('prompt')}>
+			<ClayInput
+				component="textarea"
+				onChange={({target}) =>
+					setSelectedItem({
+						...selectedItem,
+						data: {
+							...selectedItem.data,
+							prompt: target.value,
+						},
+					})
+				}
+				type="text"
+				value={selectedItem?.data.prompt ?? ''}
+			/>
 		</SidebarPanel>
 	);
 };
