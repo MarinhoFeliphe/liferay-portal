964fd94fb57b7467960990bdf05158cf3199b72c
Feliphe Marinho <feliphemarinho18@gmail.com>
2025-12-21 17:27:14 -0300

    test

--- modules/dxp/apps/ai-hub/ai-hub-rest-test/src/testIntegration/java/com/liferay/ai/hub/rest/resource/v1_0/test/TaskResourceTest.java
+++ modules/dxp/apps/ai-hub/ai-hub-rest-test/src/testIntegration/java/com/liferay/ai/hub/rest/resource/v1_0/test/TaskResourceTest.java
@@ -8,9 +8,12 @@ package com.liferay.ai.hub.rest.resource.v1_0.test;
 import com.liferay.ai.hub.rest.resource.v1_0.test.util.SseEventSourceTestUtil;
 import com.liferay.ai.hub.rest.resource.v1_0.util.SseUtil;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.object.field.builder.LongTextObjectFieldBuilder;
+import com.liferay.object.field.builder.TextObjectFieldBuilder;
 import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.service.ObjectDefinitionLocalService;
 import com.liferay.object.service.ObjectEntryLocalService;
+import com.liferay.object.test.util.ObjectDefinitionTestUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
@@ -38,22 +41,19 @@ import com.liferay.portal.workflow.constants.WorkflowDefinitionConstants;
 import com.liferay.portal.workflow.manager.WorkflowDefinitionManager;
 import com.liferay.site.initializer.SiteInitializer;
 import com.liferay.site.initializer.SiteInitializerRegistry;
-
-import java.io.InputStream;
-import java.io.Serializable;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.io.InputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Feliphe Marinho
@@ -65,6 +65,8 @@ import org.junit.runner.RunWith;
 )
 @RunWith(Arquillian.class)
 public class TaskResourceTest extends BaseTaskResourceTestCase {
+
+	private static ObjectDefinition _objectDefinition;
 
 	@BeforeClass
 	public static void setUpClass() throws Exception {
@@ -81,14 +83,29 @@ public class TaskResourceTest extends BaseTaskResourceTestCase {
 
 		_group = GroupTestUtil.addGroup();
 
-		_objectDefinition =
+		_mcpServerObjectDefinition =
 			_objectDefinitionLocalService.
 				getObjectDefinitionByExternalReferenceCode(
 					"L_MCP_SERVER", TestPropsValues.getCompanyId());
 
+		_objectDefinition = ObjectDefinitionTestUtil.publishObjectDefinition(
+			List.of(
+				new LongTextObjectFieldBuilder(
+				).labelMap(
+					RandomTestUtil.randomLocaleStringMap()
+				).name(
+					"description"
+				).build(),
+				new TextObjectFieldBuilder(
+				).labelMap(
+					RandomTestUtil.randomLocaleStringMap()
+				).name(
+					"name"
+				).build()));
+
 		_objectEntryLocalService.addObjectEntry(
 			_group.getGroupId(), TestPropsValues.getUserId(),
-			_objectDefinition.getObjectDefinitionId(), 0,
+			_mcpServerObjectDefinition.getObjectDefinitionId(), 0,
 			LocaleUtil.toLanguageId(LocaleUtil.getDefault()),
 			HashMapBuilder.<String, Serializable>put(
 				"authArguments",
@@ -117,6 +134,10 @@ public class TaskResourceTest extends BaseTaskResourceTestCase {
 				"ai-decision-node-with-tool-workflow-definition.json"));
 		_workflowDefinitionManager.deployWorkflowDefinition(
 			null, TestPropsValues.getCompanyId(), TestPropsValues.getUserId(),
+			StringUtil.randomId(), "LLM Node With RAG Workflow Definition",
+			_getContentBytes("llm-node-with-rag-workflow-definition.json"));
+		_workflowDefinitionManager.deployWorkflowDefinition(
+			null, TestPropsValues.getCompanyId(), TestPropsValues.getUserId(),
 			StringUtil.randomId(), "LLM Node With Tool Workflow Definition",
 			_getContentBytes("llm-node-with-tool-workflow-definition.json"));
 		_workflowDefinitionManager.deployWorkflowDefinition(
@@ -127,6 +148,8 @@ public class TaskResourceTest extends BaseTaskResourceTestCase {
 
 	@AfterClass
 	public static void tearDownClass() throws PortalException {
+		_objectDefinitionLocalService.deleteObjectDefinition(
+			_mcpServerObjectDefinition.getObjectDefinitionId());
 		_objectDefinitionLocalService.deleteObjectDefinition(
 			_objectDefinition.getObjectDefinitionId());
 
@@ -146,15 +169,15 @@ public class TaskResourceTest extends BaseTaskResourceTestCase {
 				List.of(), new ArrayList<>(), "tasks/subscribe"));
 	}
 
-	@Ignore
 	@Override
 	@Test
 	public void testPostByExternalReferenceCodeTask() throws Exception {
-		_testPostByExternalReferenceCodeTask();
+		/*_testPostByExternalReferenceCodeTask();
 		_testPostByExternalReferenceCodeTaskWithScope();
 		_testPostByExternalReferenceCodeTaskWithTypeAIDecisionNodeWithToolWorkflowDefinition();
 		_testPostByExternalReferenceCodeTaskWithTypeAIDecisionNodeWorkflowDefinition();
-		_testPostByExternalReferenceCodeTaskWithTypeFixSpellingAndGrammar();
+		_testPostByExternalReferenceCodeTaskWithTypeFixSpellingAndGrammar();*/
+		_testPostByExternalReferenceCodeTaskWithTypeLLMNodeWithRAGWorkflowDefinition();
 		_testPostByExternalReferenceCodeTaskWithTypeLLMNodeWithToolWorkflowDefinition();
 	}
 
@@ -331,6 +354,80 @@ public class TaskResourceTest extends BaseTaskResourceTestCase {
 		Assert.assertEquals("event: Fix Spelling and Grammar", lines.get(2));
 		Assert.assertEquals(
 			"data: {\"data\":\"This text is wrong.\"}", lines.get(3));
+
+		SseUtil.closeAll();
+	}
+
+	private void _testPostByExternalReferenceCodeTaskWithTypeLLMNodeWithRAGWorkflowDefinition()
+		throws Exception {
+
+		CountDownLatch countDownLatch1 = new CountDownLatch(4);
+		CountDownLatch countDownLatch2 = new CountDownLatch(6);
+		List<String> lines = new ArrayList<>();
+
+		String sseEventSinkKey = SseEventSourceTestUtil.open(
+			List.of(countDownLatch1, countDownLatch2), lines, "tasks/subscribe");
+
+		HTTPTestUtil.invokeToJSONObject(
+			JSONUtil.put(
+				"context",
+				JSONUtil.put(
+					"userMessage", "What is Feliphe's favorite food?")
+			).put(
+				"scope",
+				JSONUtil.put(
+					"externalReferenceCode", _group.getExternalReferenceCode())
+			).put(
+				"type", "LLM Node With RAG Workflow Definition"
+			).toString(),
+			"ai-hub/v1.0/by-external-reference-code/" + sseEventSinkKey +
+			"/tasks",
+			Http.Method.POST);
+
+		Assert.assertTrue(countDownLatch1.await(100, TimeUnit.SECONDS));
+
+		Assert.assertEquals(lines.toString(), 4, lines.size());
+
+		String response = StringUtil.toLowerCase(lines.get(3));
+
+		Assert.assertFalse(response, response.contains("brazilian barbecue"));
+
+		_objectEntryLocalService.addObjectEntry(
+			0L, TestPropsValues.getUserId(),
+			_objectDefinition.getObjectDefinitionId(), 0,
+			LocaleUtil.toLanguageId(LocaleUtil.getDefault()),
+			HashMapBuilder.<String, Serializable>put(
+				"description", "His favorite food is Brazilian barbecue."
+			).put(
+				"name", "Feliphe"
+			).build(),
+			ServiceContextTestUtil.getServiceContext());
+
+		HTTPTestUtil.invokeToJSONObject(
+			JSONUtil.put(
+				"context",
+				JSONUtil.put(
+					"userMessage", "What is Feliphe's favorite food?")
+			).put(
+				"scope",
+				JSONUtil.put(
+					"externalReferenceCode", _group.getExternalReferenceCode())
+			).put(
+				"type", "LLM Node With RAG Workflow Definition"
+			).toString(),
+			"ai-hub/v1.0/by-external-reference-code/" + sseEventSinkKey +
+			"/tasks",
+			Http.Method.POST);
+
+		Assert.assertTrue(countDownLatch2.await(100, TimeUnit.SECONDS));
+
+		Assert.assertEquals(lines.toString(), 6, lines.size());
+
+		response = StringUtil.toLowerCase(lines.get(3));
+
+		Assert.assertTrue(response, response.contains("brazilian barbecue"));
+
+		SseUtil.closeAll();
 	}
 
 	private void _testPostByExternalReferenceCodeTaskWithTypeLLMNodeWithToolWorkflowDefinition()
@@ -365,13 +462,15 @@ public class TaskResourceTest extends BaseTaskResourceTestCase {
 		String response = StringUtil.toLowerCase(lines.get(3));
 
 		Assert.assertTrue(response, response.contains("yes"));
+
+		SseUtil.closeAll();
 	}
 
 	@Inject
 	private static ClassNameLocalService _classNameLocalService;
 
 	private static Group _group;
-	private static ObjectDefinition _objectDefinition;
+	private static ObjectDefinition _mcpServerObjectDefinition;
 
 	@Inject
 	private static ObjectDefinitionLocalService _objectDefinitionLocalService;
