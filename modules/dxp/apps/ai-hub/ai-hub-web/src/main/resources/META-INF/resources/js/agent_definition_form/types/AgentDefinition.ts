9e997b78bdb5fd260f800c79460816f3757023a7
Davyson Melo <davyson.melo@liferay.com>
2026-02-13 16:10:43 -0300

    LPD-78064 feat: create AgentDefinition type

---
+++ modules/dxp/apps/ai-hub/ai-hub-web/src/main/resources/META-INF/resources/js/agent_definition_form/types/AgentDefinition.ts
@@ -0,0 +1,17 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+export type AgentDefinition = {
+    active: boolean;
+    category?: string;
+    description: string;
+    externalReferenceCode: string;
+    inputVariables: string;
+    outputVariables: string;
+    title_i18n: {
+        [key: string]: string;
+    };
+    workflowDefinitionName?: string;
+}
