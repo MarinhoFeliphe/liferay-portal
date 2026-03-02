2ecb50c54985867a3e112301d12a0aef0c526cd1
Feliphe Marinho <feliphemarinho18@gmail.com>
2026-03-02 18:05:17 -0300

    LPD-78054 fix: use selectedKey in order to set a predefined value
    Co-authored-by: Joao Victor <joao.victor@liferay.com>

--- modules/dxp/apps/ai-hub/ai-hub-web/src/main/resources/META-INF/resources/js/agent_definition_form/AgentDefinitionForm.tsx
+++ modules/dxp/apps/ai-hub/ai-hub-web/src/main/resources/META-INF/resources/js/agent_definition_form/AgentDefinitionForm.tsx
@@ -388,7 +388,7 @@ export default function AgentDefinitionForm({
 												'workflow-definition'
 											)}
 											required={true}
-											value={
+											selectedKey={
 												formData.workflowDefinitionName
 											}
 										>
