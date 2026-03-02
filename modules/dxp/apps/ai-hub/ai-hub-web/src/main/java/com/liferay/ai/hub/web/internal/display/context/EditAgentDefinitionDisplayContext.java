7476b4c3199f90737c2ff11dfa442990859b4705
Feliphe Marinho <feliphemarinho18@gmail.com>
2026-03-02 15:48:08 -0300

    LPD-78054 feat: pass the accountEntryExternalReferenceCode parameter and set the groupExternalReferenceCode in the workflow definition url
    Co-authored-by: Joao Victor <joao.victor@liferay.com>

--- modules/dxp/apps/ai-hub/ai-hub-web/src/main/java/com/liferay/ai/hub/web/internal/display/context/EditAgentDefinitionDisplayContext.java
+++ modules/dxp/apps/ai-hub/ai-hub-web/src/main/java/com/liferay/ai/hub/web/internal/display/context/EditAgentDefinitionDisplayContext.java
@@ -5,15 +5,19 @@
 
 package com.liferay.ai.hub.web.internal.display.context;
 
+import com.liferay.account.model.AccountEntry;
+import com.liferay.ai.hub.util.AccountEntryUtil;
 import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.GroupConstants;
+import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.HttpComponentsUtil;
 import com.liferay.portal.kernel.util.Portal;
-import com.liferay.portal.kernel.util.PropsValues;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.workflow.constants.WorkflowDefinitionConstants;
 import com.liferay.portal.workflow.constants.WorkflowPortletKeys;
@@ -31,8 +35,10 @@ import java.util.Map;
 public class EditAgentDefinitionDisplayContext {
 
 	public EditAgentDefinitionDisplayContext(
+		GroupLocalService groupLocalService,
 		HttpServletRequest httpServletRequest, Portal portal) {
 
+		_groupLocalService = groupLocalService;
 		_httpServletRequest = httpServletRequest;
 		_portal = portal;
 
@@ -41,22 +47,38 @@ public class EditAgentDefinitionDisplayContext {
 	}
 
 	public Map<String, Object> getReactData() throws Exception {
+		AccountEntry accountEntry = AccountEntryUtil.getUserAccountEntry(
+			_themeDisplay.getUserId());
+
+		Group group = _groupLocalService.getGroup(
+			_themeDisplay.getScopeGroupId());
+
 		Company company = _themeDisplay.getCompany();
 
-		String portalURL = company.getPortalURL(
-			GroupConstants.DEFAULT_PARENT_GROUP_ID);
-
-		String workflowDefinitionName = _httpServletRequest.getParameter(
-			"workflowDefinitionName");
+		String aiHubURL = StringBundler.concat(
+			company.getPortalURL(GroupConstants.DEFAULT_PARENT_GROUP_ID),
+			"/web", group.getFriendlyURL());
 
 		return HashMapBuilder.<String, Object>put(
-			"backURL", portalURL + "/web/ai-hub/agents"
+			"accountEntryExternalReferenceCode",
+			() -> {
+				if (accountEntry == null) {
+					return null;
+				}
+
+				return accountEntry.getExternalReferenceCode();
+			}
+		).put(
+			"backURL", aiHubURL + "/agents"
 		).put(
 			"externalReferenceCode",
 			_httpServletRequest.getParameter("externalReferenceCode")
 		).put(
 			"readonly",
 			() -> {
+				String workflowDefinitionName =
+					_httpServletRequest.getParameter("workflowDefinitionName");
+
 				if ((workflowDefinitionName != null) &&
 					ArrayUtil.contains(
 						WorkflowDefinitionConstants.
@@ -74,22 +96,13 @@ public class EditAgentDefinitionDisplayContext {
 				String namespace = _portal.getPortletNamespace(
 					WorkflowPortletKeys.KALEO_DESIGNER);
 
-				String url = StringBundler.concat(
-					portalURL,
-					PropsValues.
-						LAYOUT_FRIENDLY_URL_PRIVATE_GROUP_SERVLET_MAPPING,
-					GroupConstants.CONTROL_PANEL_FRIENDLY_URL,
-					PropsValues.CONTROL_PANEL_LAYOUT_FRIENDLY_URL);
-
-				if (workflowDefinitionName != null) {
-					url = HttpComponentsUtil.addParameter(
-						url, namespace + "name", workflowDefinitionName);
-				}
+				String url = _addGroupExternalReferenceCodeParameter(
+					accountEntry, namespace, aiHubURL + "/workflow-definition");
 
 				return HttpComponentsUtil.addParameters(
-					url, "p_p_id", WorkflowPortletKeys.KALEO_DESIGNER,
-					"p_p_lifecycle", "0", "p_p_state",
-					WindowState.MAXIMIZED.toString(), "p_p_mode",
+					_addNameParameter(namespace, url), "p_p_id",
+					WorkflowPortletKeys.KALEO_DESIGNER, "p_p_lifecycle", "0",
+					"p_p_state", WindowState.MAXIMIZED.toString(), "p_p_mode",
 					PortletMode.VIEW.toString(), namespace + "mvcPath",
 					"/designer/edit_workflow_definition.jsp",
 					namespace + "redirect",
@@ -101,6 +114,35 @@ public class EditAgentDefinitionDisplayContext {
 		).build();
 	}
 
+	private String _addGroupExternalReferenceCodeParameter(
+			AccountEntry accountEntry, String namespace, String url)
+		throws PortalException {
+
+		if (accountEntry == null) {
+			return url;
+		}
+
+		Group group = _groupLocalService.getGroup(
+			accountEntry.getAccountEntryGroupId());
+
+		return HttpComponentsUtil.addParameter(
+			url, namespace + "groupExternalReferenceCode",
+			group.getExternalReferenceCode());
+	}
+
+	private String _addNameParameter(String namespace, String url) {
+		String workflowDefinitionName = _httpServletRequest.getParameter(
+			"workflowDefinitionName");
+
+		if (workflowDefinitionName == null) {
+			return url;
+		}
+
+		return HttpComponentsUtil.addParameter(
+			url, namespace + "name", workflowDefinitionName);
+	}
+
+	private final GroupLocalService _groupLocalService;
 	private final HttpServletRequest _httpServletRequest;
 	private final Portal _portal;
 	private final ThemeDisplay _themeDisplay;
